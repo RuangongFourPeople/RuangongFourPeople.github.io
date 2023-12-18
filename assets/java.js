45c883aebfb0bc25a8ad17a6351b12c4f11179a0
233MAKA <jx3176@stu.ouc.edu.cn>
2023-12-18 10:08:22 +0800

    2021.12.18 下载接口

--- assets/java.js
+++ assets/java.js
@@ -1,22 +1,24 @@
+function upload() {
+    var file = document.querySelector('[type=file]').files[0];
+    var formData = new FormData();
+    formData.append('file', file);
 
-function upload () {
-    var file = document.querySelector('[type=file]')
-    var fileReader = new FileReader()
-    // file.files[0]是值第一个文件，这里调用readAsArrayBuffer是读成二进制流数组
-    fileReader.readAsArrayBuffer(file.files[0])
-    // fileReader读取文件并转化二进制流需要时间，在读取结束后就可以调用filereader实例的result属性查看读取的二进制流数组
-    fileReader.onload = function () {
-    // 直接使用axios上传
-    axios.post('http://localhost:4000/uploadFile?fileName=' + file.files[0].name, fileReader.result).then(res => {
-        //上传成功
-            console.log(res.data);
-            alert("文件上传成功");
-            document.getElementById("message").innerText = "文件上传成功"; // 在界面上显示上传成功的消息
-        }).catch(error => {
-            //上传失败
-            console.error('文件上传失败', error);
-            alert("文件上传失败");
-            document.getElementById("message").innerText = "文件上传失败"; // 在界面上显示上传失败的消息
-        });
-    }
-  }
+    // 使用fetch或axios将文件发送到后端
+    fetch('http://localhost:8080/upload', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        // 处理后端的响应
+        console.log(data);
+        alert("文件上传成功");
+        document.getElementById("message").innerText = "文件上传成功";
+    })
+    .catch(error => {
+        // 处理请求过程中出现的任何错误
+        console.error('文件上传失败', error);
+        alert("文件上传失败");
+        document.getElementById("message").innerText = "文件上传失败";
+    });
+}
